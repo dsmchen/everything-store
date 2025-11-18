af391121627848b3d7bfddb381b802064375d7b5
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-11-18 14:21:01 +0800

    feat: add star rating

--- js/components/product-grid.js
+++ js/components/product-grid.js
@@ -8,33 +8,63 @@ export async function displayProducts() {
     const productCard = document.createElement('article');
     const productTitle = document.createElement('h2');
     const productPrice = document.createElement('span');
-    const productRating = document.createElement('span');
     const productCategory = document.createElement('span');
     const productImage = document.createElement('img');
-    const productTextContainer = document.createElement('div');
     const productImageContainer = document.createElement('div');
+    const productRatingRate = document.createElement('span');
+    const productRatingCount = document.createElement('span');
+    const productRatingContainer = document.createElement('div');
 
     productTitle.classList.add('title');
     productPrice.classList.add('price');
-    productRating.classList.add('rating');
     productCategory.classList.add('category');
     productImage.classList.add('image');
-    productTextContainer.classList.add('text-container');
     productImageContainer.classList.add('image-container');
+    productRatingRate.classList.add('rating-rate');
+    productRatingCount.classList.add('rating-count');
+    productRatingContainer.classList.add('rating-container');
 
     productTitle.textContent = product.title;
     productPrice.textContent = `$${product.price}`;
-    productRating.textContent = `${product.rating.rate}/5 (${product.rating.count})`;
     productCategory.textContent = product.category;
     productImage.src = product.image;
     productImage.alt = product.title;
+    productRatingCount.textContent = `(${product.rating.count})`;
 
-    productTextContainer.append(productRating, productPrice);
+    let stars = 0;
+
+    for (let i = 1; i < product.rating.rate * 10; i++) {
+      if (i % 10 === 0) {
+        const starFill = document.createElement('span');
+        starFill.classList.add('star-fill', 'star');
+        productRatingRate.appendChild(starFill);
+        stars++;
+      }
+    }
+
+    const remainder = (product.rating.rate * 10) % 10;
+    if (remainder >= 5) {
+      const starHalf = document.createElement('span');
+      starHalf.classList.add('star-half', 'star');
+      productRatingRate.appendChild(starHalf);
+      stars++;
+    }
+
+    if (stars < 5) {
+      for (let i = 0; i < 5 - stars; i++) {
+        const starLine = document.createElement('span');
+        starLine.classList.add('star-line', 'star');
+        productRatingRate.appendChild(starLine);
+      }
+    }
+
     productImageContainer.appendChild(productImage);
+    productRatingContainer.append(productRatingRate, productRatingCount);
     productCard.append(
       productImageContainer,
-      productTextContainer,
       productTitle,
+      productPrice,
+      productRatingContainer,
       productCategory
     );
     productGrid.appendChild(productCard);
