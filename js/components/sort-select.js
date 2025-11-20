a49df5fa1eb3710c38233173da3872ffc1b3f9b8
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-11-20 16:50:53 +0800

    refactor: move compare functions

--- js/components/sort-select.js
+++ js/components/sort-select.js
@@ -4,29 +4,19 @@ export function initSortSelect() {
   function handleSelect() {
     if (sortSelect.value) {
       const products = document.querySelectorAll('article');
+      const productsArray = Array.from(products);
       const productGrid = document.getElementById('product-grid');
       let sortedProducts = [];
 
       switch (sortSelect.value) {
         case 'price-low':
-          function comparePriceLow(a, b) {
-            return a.getAttribute('data-price') - b.getAttribute('data-price');
-          }
-          sortedProducts = Array.from(products).sort(comparePriceLow);
+          sortedProducts = productsArray.sort(comparePriceLow);
           break;
         case 'price-high':
-          function comparePriceHigh(a, b) {
-            return b.getAttribute('data-price') - a.getAttribute('data-price');
-          }
-          sortedProducts = Array.from(products).sort(comparePriceHigh);
+          sortedProducts = productsArray.sort(comparePriceHigh);
           break;
         case 'rating':
-          function compareRating(a, b) {
-            return (
-              b.getAttribute('data-rating') - a.getAttribute('data-rating')
-            );
-          }
-          sortedProducts = Array.from(products).sort(compareRating);
+          sortedProducts = productsArray.sort(compareRating);
           break;
         default:
           const p = document.createElement('p');
@@ -35,10 +25,20 @@ export function initSortSelect() {
           productGrid.appendChild(p);
       }
 
-      sortedProducts.forEach((product) => {
-        productGrid.appendChild(product);
-      });
+      sortedProducts.forEach((product) => productGrid.appendChild(product));
     }
+  }
+
+  function comparePriceLow(a, b) {
+    return a.getAttribute('data-price') - b.getAttribute('data-price');
+  }
+
+  function comparePriceHigh(a, b) {
+    return b.getAttribute('data-price') - a.getAttribute('data-price');
+  }
+
+  function compareRating(a, b) {
+    return b.getAttribute('data-rating') - a.getAttribute('data-rating');
   }
 
   sortSelect.addEventListener('change', handleSelect);
