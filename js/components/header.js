9ace61f842438b1f084fdda7d4fbb7e2209b578a
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-11-20 15:55:14 +0800

    refactor: add showSearchResults function

--- js/components/header.js
+++ js/components/header.js
@@ -1,21 +1,25 @@
 export function search() {
-  const searchInput = document.getElementById('product-search');
   const submitBtn = document.querySelector('form button');
 
   function handleSubmit(e) {
     e.preventDefault();
 
     const products = document.querySelectorAll('article');
-    Array.from(products).map((product) => {
-      product.classList.remove('hidden');
-      const title = product.getAttribute('data-title').toLowerCase();
-      const desc = product.getAttribute('data-desc').toLowerCase();
-      const keywords = searchInput.value.toLowerCase();
+    const searchInput = document.getElementById('product-search');
+    const keywords = searchInput.value.toLowerCase();
 
-      if (!title.includes(keywords) && !desc.includes(keywords)) {
-        product.classList.add('hidden');
-      }
-    });
+    Array.from(products).map((product) => showSearchResults(product, keywords));
+  }
+
+  function showSearchResults(product, keywords) {
+    const title = product.getAttribute('data-title').toLowerCase();
+    const desc = product.getAttribute('data-desc').toLowerCase();
+
+    product.classList.remove('hidden');
+
+    if (!title.includes(keywords) && !desc.includes(keywords)) {
+      product.classList.add('hidden');
+    }
   }
 
   submitBtn.addEventListener('click', handleSubmit);
